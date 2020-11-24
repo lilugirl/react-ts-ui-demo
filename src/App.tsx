2d159b76bd88f7a12f4ecda78bb5598789d24ea3
liuyi <794516454@qq.com>
2020-11-25 02:44:17 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Button , {ButtonType,ButtonSize}from './components/Button/button'
+
+
+
+function App() {
+
+ 
+  return (
+    <div className="App">
+      app
+      <Button btnType={ButtonType.Danger} size={ButtonSize.Small} >按钮</Button>
+      <Button disabled >按钮</Button>
+      <Button btnType={ButtonType.Primary}  size={ButtonSize.Large}>按钮</Button>
+      <Button btnType={ButtonType.Link} href="http://126.com" >Link</Button>
+      <Button btnType={ButtonType.Link} href="http://126.com" disabled>Link</Button>
+      <h1>Hello World</h1>
+      <h2>Hello World</h2>
+      <h3>Hello World</h3>
+      <hr/>
+      <code>
+        const a = 'b'
+      </code>
+      <p>Edit <code>src/App.tsx</code> and save to reload</p>
+      <a href="#" >Links</a>
+    </div>
+  );
+}
+
+export default App;
